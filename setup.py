275a5d0d82259eaafdcf4c168bdfde6fd5631ad4
Pendy <pendy40u@gmail.com>
2018-07-02 11:07:59 -0400

    Initial controller commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='labjackcontroller',
+      version='0.1',
+      description='A helper library to control LabJack devices',
+      packages=['labjackcontroller'],
+      licence='MIT',
+      install_requires=[
+                        'typing',
+                        'numpy',
+                        'math',
+                        'time',
+                        'multiprocessing'
+                        ],
+      zip_safe=False)
