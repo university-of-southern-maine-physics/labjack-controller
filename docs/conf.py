4fc68c3f180025d37858a19008dc57251b480ba7
Ben Montgomery <ben_mon@outlook.com>
2019-04-06 17:57:36 -0400

    Corrected abspath

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ import os
 import sys
 import guzzle_sphinx_theme
 
-sys.path.insert(0, os.path.abspath("../../.."))
+sys.path.insert(0, os.path.abspath(".."))
 
 
 # -- Project information -----------------------------------------------------
