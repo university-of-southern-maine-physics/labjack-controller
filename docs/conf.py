4d412ec35454334e114be3723ff67d0fe31c3c5b
Ben Montgomery <ben_mon@outlook.com>
2019-05-06 14:18:46 -0400

    Added NumPy referencing for docs

--- docs/conf.py
+++ docs/conf.py
@@ -199,4 +199,5 @@ epub_exclude_files = ['search.html']
 # -- Options for intersphinx extension ---------------------------------------
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {'python': ('https://docs.python.org/3', None),
+                       'numpy': ('http://docs.scipy.org/doc/numpy', None)}
